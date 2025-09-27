4c47c73ad3cf773054f401cce4ddcc53277ee294
Krish Sangani <158293454+krishpatel09@users.noreply.github.com>
2025-09-27 19:02:33 +0530

    Update CORS configuration to accept an array of origins for enhanced flexibility

--- back-end/server.js
+++ back-end/server.js
@@ -21,7 +21,7 @@ app.use(limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: [process.env.FRONTEND_URL],
   credentials: true
 }));
 
